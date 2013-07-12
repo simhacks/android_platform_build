1f33f0fdb622e32e59178f10064f998c436020c5
Eric Butler <eric@codebutler.com>
2013-07-12 12:43:38 -0700

    Include Stk app in build.
    Change-Id: I5f63732a192bf8051eb3e4751401ad8b24aca40a

--- target/product/core.mk
+++ target/product/core.mk
@@ -23,6 +23,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.alarm_alert=Alarm_Classic.ogg
 
 PRODUCT_PACKAGES += \
+    Stk \
     SmartcardService \
     org.simalliance.openmobileapi \
     org.simalliance.openmobileapi.xml \
